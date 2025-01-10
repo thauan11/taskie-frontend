98aab4889d49e22103afdbf8098935a82ce79ae6
Victor Andrade <victor-tandrade@hotmail.com>
2025-01-09 23:56:32 -0300

    remove user.tsx

--- src/components/modalLogout.tsx
+++ src/components/modalLogout.tsx
@@ -1,26 +1,25 @@
 "use client";
 import { deleteCookie } from "@/hooks/deleteCookie";
 import { useRouter } from "next/navigation";
-import { UserPortrait } from "./user";
 import { useEffect, useState } from "react";
 import Loading from "./loading";
 import { useUser } from "@/hooks/useUser";
+import Image from "next/image";
 
 export function ModalLogout() {
-  const [showLogout, setShowLogout] = useState(false);
+	const [showLogout, setShowLogout] = useState(false);
 	const [loading, setLoading] = useState(false);
 
 	const router = useRouter();
-  const { user } = useUser();
+	const { user } = useUser();
 
 	const splitText = (text: string) => {
-		return text.split(' ')[0]
-	}
+		return text.split(" ")[0];
+	};
 
 	const handleLogout = async () => {
 		setLoading(true);
 		await deleteCookie("authToken");
-		await deleteCookie("token");
 		router.push("/login");
 	};
 
@@ -32,18 +31,46 @@ export function ModalLogout() {
 				</div>
 			)}
 
-			{showLogout &&
-				<button type="button" className="absolute top-0 left-0 w-full h-full bg-white opacity-0 cursor-default" onClick={() => setShowLogout(!showLogout)}/>
-			}
-			
-			{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-			<div
-				className="relative flex justify-center min-h-8 text-sm cursor-pointer"
-				onClick={() => setShowLogout(!showLogout)}
-			>
-				<UserPortrait size="sm" />
+			{showLogout && (
+				<button
+					type="button"
+					className="absolute top-0 left-0 w-full h-full bg-white opacity-0 cursor-default"
+					onClick={() => setShowLogout(false)}
+				/>
+			)}
 
-				{showLogout &&
+			<button
+				type="button"
+				className="relative flex justify-center min-h-8 text-sm"
+				onClick={() => setShowLogout(true)}
+			>
+				<div className="relative w-8 h-8 rounded-full">
+					{user?.portrait ? (
+						<Image
+							src={user.portrait}
+							alt={`${user?.name}'s portrait`}
+							fill
+							className="object-cover"
+						/>
+					) : (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="currentColor"
+							className="w-8 h-8 object-cover"
+						>
+							{/* <title>{splitText(user?.name as string)}'s portrait</title> */}
+							<title> </title>
+							<path
+								fillRule="evenodd"
+								d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+								clipRule="evenodd"
+							/>
+						</svg>
+					)}
+				</div>
+
+				{showLogout && (
 					<div className="absolute top-10 right-[-0.5rem] bg-zinc-600 w-32 p-2 flex flex-col justify-center">
 						<div className="border-b border-dotted text-center pb-2 mb-2">
 							{splitText(user?.name as string)}'s Taskie
@@ -53,8 +80,8 @@ export function ModalLogout() {
 							Logout
 						</button>
 					</div>
-				}
-			</div>
+				)}
+			</button>
 		</>
 	);
 }
